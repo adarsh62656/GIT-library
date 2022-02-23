b10babb3708226c4e553632fadd8cb1bc69f87dc
adarsh62656 <adarsh62656@gmail.com>
2022-02-23 15:20:33 +0530

    modified git.py

--- git.py
+++ git.py
@@ -1,108 +1,44 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import subprocess
-
-
-# In[2]:
-
-
-print(subprocess.check_output(['echo' ,'%cd%'],shell=True).decode('utf-8'))
-
-
-# ## Git Clone
-
-# In[4]:
-
-
+import os
 def clone(path):
-    subprocess.call(['git','clone',path])
-
-
-# In[28]:
-
-
-clone("https://github.com/adarsh62656/PHPapp.git")
-
-
-# ## Git Add
-
-# In[29]:
-
-
+    x=subprocess.Popen(['git','clone',path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = x.communicate()
+    print(stdout.decode('utf-8'), stderr.decode('utf-8'))
 def addtostage(path):
     """path -   Add Path of file to Move on staging area"""
-    subprocess.call(['git','add',path])
-
-
-# ## Git rm
-
-# In[36]:
-
-
+    x=subprocess.Popen(['git','add',path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = x.communicate()
+    print(stdout.decode('utf-8'), stderr.decode('utf-8'))
+def pri(process):
+    stdout, stderr = process.communicate()
+    print(stdout.decode('utf-8'), stderr.decode('utf-8'))
 def rmstage(path):
     """path -   Add Path of file to remove from staging area"""
-    subprocess.call(['git','rm',path])
-
-
-# ## Git Commit
-
-# In[30]:
-
-
+    pri(subprocess.Popen(['git','rm','-f',path], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+    print(path,": Removed Successfuly")
 def commit(message):
     """message -   Enter Commit Message"""
-    subprocess.call(['git','commit','-m',message])
-
-
-# ## Git INIT
-
-# In[51]:
-
-
+    pri(subprocess.Popen(['git','commit','-m',message], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
 def gitinit(path=""):
     """path -   Enter Path to initialize empty repozitory"""
     if path=="":
-        subprocess.call(['git','init'])
+        pri(subprocess.Popen(['git','init'], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
     else:
-        subprocess.call(['git','init',path])
-
-
-# ## Git log
-
-# In[52]:
-
-
-import os
+        pri(subprocess.call(['git','init',path], stdout=subprocess.PIPE, stderr=subprocess.PIPE))
 def log(path=""):
     """path -   Enter Path to see logs of repozitory"""
     os. chdir(path)
     print(subprocess.check_output(['git' ,'log'],shell=True).decode('utf-8'))
-
-
-# In[53]:
-
-
-
-
-
-# In[61]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
+def add_origin(origin,path):
+    """origin -  add the remote point name """
+    """url    -  add url to connect with remote repositary"""
+    subprocess.call(['git','remote','add',origin,path])
+def git_push(origin,branch):
+    """origin -  mantion the remote point name to push"""
+    """branch -  branch name from where to push"""
+    subprocess.call(['git','push',origin,branch])
+def status(path=""):
+    """path -   Enter Path to see logs of repozitory"""
+    if not path=="":
+        os. chdir(path)
+    print(subprocess.check_output(['git' ,'status'],shell=True).decode('utf-8'))
